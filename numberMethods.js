733c0fd497869a8389fcf82abe4de15395fbb99e
Karima-Aktari <karimaaktari911@gmail.com>
2022-06-04 22:28:09 +0600

    string

---
+++ numberMethods.js
@@ -0,0 +1,13 @@
+//toString();
+let x = 594;
+const y = x.toString();
+console.log(y);
+
+const z = typeof (y);
+console.log(z);
+
+//toExponential();
+let a = 45.54;
+
+let b = a.toExponential(1);
+console.log(b);
--- stringToFixed.js
+++ stringToFixed.js
@@ -3,5 +3,5 @@ let price = 10;
 let VAT = 0.25;
 
 let total = `Total: ${(price * (1 + VAT)).toFixed(4)}`;
-// const total1 = typeOf(total);
-console.log(total);
+const total1 = typeof total;
+console.log(total1);
