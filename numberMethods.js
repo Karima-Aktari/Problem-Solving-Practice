8e23d1e62b9adab1ddc0b4b2ee379cc426d158d0
Karima-Aktari <karimaaktari911@gmail.com>
2022-06-05 11:55:17 +0600

    number

--- numberMethods.js
+++ numberMethods.js
@@ -1,13 +1,41 @@
-//toString();
+//1. toString();
 let x = 594;
 const y = x.toString();
-console.log(y);
+// console.log(y);
 
 const z = typeof (y);
-console.log(z);
+// console.log(z);
 
-//toExponential();
+//2. toExponential();
 let a = 45.54;
 
 let b = a.toExponential(1);
-console.log(b);
+// console.log(b);
+
+//3. toFixed();
+let e = 9.76432;
+const f = e.toFixed();
+console.log(typeof (f));
+// console.log(e.toFixed(2));
+// console.log(e.toFixed(5));
+
+
+//4. toPrecision();
+const g = e.toPrecision(2);
+// console.log(g);
+// console.log(typeof (g));
+
+// 5. Number ();
+
+let h = (true);
+let h1 = (false);
+let h2 = ("25");
+let h3 = ("24.678");
+console.log(Number(h));
+console.log(Number(h1));
+console.log(Number(h2));
+console.log(Number(h3));
+
+let h4 = new Date("2017-09-30");
+console.log(Number(h4));
+console.log(h4);
