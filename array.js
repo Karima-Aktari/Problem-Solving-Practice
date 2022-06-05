6f126c26f1eadb30e1a52935d9dee0d4fc8e4fd7
Karima-Aktari <karimaaktari911@gmail.com>
2022-06-05 20:47:17 +0600

    array

---
+++ array.js
@@ -0,0 +1,23 @@
+//First
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.push("Lemon"); //Logic-1
+console.log(fruits);
+
+fruits[fruits.length] = "Papaya"; //Logic-2
+console.log(fruits);
+
+fruits[6] = "Lichi";//Logic-3
+console.log(fruits);
+
+console.log(fruits.length);
+
+const type = typeof fruits;
+console.log(type);
+
+//concat() method;
+const arr1 = ["Cecilie", "Lone"];
+const arr2 = ["Emil", "Tobias", "Linus"];
+const arr3 = ["Robin", "Morgan"];
+const person = arr1.concat(arr2, arr3);
+console.log(person);
