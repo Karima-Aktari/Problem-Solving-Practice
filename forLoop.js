91fb1df599c8e266223e9e55a2c9b61cc75f02ca
Karima-Aktari <karimaaktari911@gmail.com>
2022-06-06 21:08:50 +0600

    forloop

---
+++ forLoop.js
@@ -0,0 +1,23 @@
+
+let first = [12, 21, 23, 11, 24, 45];
+
+for (let i = 0; i < first.length; i++) {
+    let result = first[i];
+    console.log(result); //element
+    console.log(i); //index
+}
+
+//2.
+
+function loop(element) {
+
+    for (let i = 0; i < element.length; i++) {
+        let result1 = fruits[i];
+        console.log(result1);
+
+    }
+
+}
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const outPut = loop(fruits);
+
