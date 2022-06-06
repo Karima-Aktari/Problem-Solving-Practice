903a0fe4d58357ae3fadcd8a9fb66c2921476b98
Karima-Aktari <karimaaktari911@gmail.com>
2022-06-06 19:59:06 +0600

    array

--- array.js
+++ array.js
@@ -16,8 +16,16 @@ const type = typeof fruits;
 console.log(type);
 
 //concat() method;
+
 const arr1 = ["Cecilie", "Lone"];
 const arr2 = ["Emil", "Tobias", "Linus"];
 const arr3 = ["Robin", "Morgan"];
 const person = arr1.concat(arr2, arr3);
 console.log(person);
+
+// const max = Math.max(fruits.length);
+// console.log(max);
+
+const arr = [1, 2, 3];
+const max = Math.max(...arr);
+console.log(max);
